f6a1b0192b3dfd830530165e9488aa2e495d2760
Sylwia <szydasylwia@protonmail.com>
2023-05-17 18:02:16 +0200

    Flask login module and authentication

--- website/auth.py
+++ website/auth.py
@@ -16,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password.', category='error')
@@ -24,8 +25,10 @@ def login():
     return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 0
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -35,9 +38,10 @@ def sign_up():
         password = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email)
         if user:
             flash('Account already exists', category='error')
+            return redirect(url_for('auth.login'))
         if password != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password) < 5:
@@ -46,6 +50,7 @@ def sign_up():
             add_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method='sha256'))
             db.session.add(add_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created', category='success')
             return redirect(url_for('views.home'))
 
