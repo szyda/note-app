74daf05ec911963bcb3a98245826f6f57be12a1e
Sylwia <szydasylwia@protonmail.com>
2023-05-17 15:26:05 +0200

    Created Jinja templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
 from flask import Blueprint
+from flask import render_template
+from flask_login import current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", user=current_user, username="Sylwia")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return 0
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign up</p>"
+    return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html", user=current_user)
